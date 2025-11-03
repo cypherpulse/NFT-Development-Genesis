e9896e8cf744a56af55c8d46ff9c1cbaecac47f3
John-Mukhwana <johnbradill67@gmail.com>
2025-11-03 18:01:34 +0300

    Added token counter variable to BasicNft contract

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -6,5 +6,8 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721{
+    uint256 private s_tokenCounter;
+
     constructor() ERC721("Cypherpulse", "CYPHER"){}
-    }
+
+}
