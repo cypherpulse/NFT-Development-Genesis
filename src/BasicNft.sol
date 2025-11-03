97e977e5723bcf592a87c1f63077a26332094843
John-Mukhwana <johnbradill67@gmail.com>
2025-11-03 18:02:19 +0300

    Initialize token counter in BasicNft constructor

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -8,6 +8,10 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract BasicNft is ERC721{
     uint256 private s_tokenCounter;
 
-    constructor() ERC721("Cypherpulse", "CYPHER"){}
+    constructor() ERC721("Cypherpulse", "CYPHER"){
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public
 
 }
