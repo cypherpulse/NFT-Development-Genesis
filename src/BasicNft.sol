fad9784326dbacc23627700a231769c0a55ed6ed
John-Mukhwana <johnbradill67@gmail.com>
2025-11-06 10:25:30 +0300

    Update mintNft function to accept a string parameter and add mapping for token URIs

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -7,12 +7,13 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721{
     uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenIdToUri;
 
     constructor() ERC721("Cypherpulse", "CYPHER"){
         s_tokenCounter = 0;
     }
 
-    function mintNft() public{}
+    function mintNft(string memory) public{}
 
     function tokenURI(uint256 tokenId) public view override returns (string memory){
         return ""
