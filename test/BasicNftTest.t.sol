57aac1415ac0f57d042771224dc47eeb7cf8c244
John-Mukhwana <johnbradill67@gmail.com>
2025-11-21 08:32:00 +0300

    Add DeployBasicNft import and deployer variable to BasicNftTest contract

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -3,7 +3,8 @@
 pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
 
 contract BasicNftTest is Test{
-
+    DeployBasicNft public deployer;
 }
