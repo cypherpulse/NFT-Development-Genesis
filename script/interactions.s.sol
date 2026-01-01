05063262ed88264c8c3c11a420976e77330f5423
cypherpulse <johnbradill67@gmail.com>
2026-01-01 08:59:05 +0300

    NFT Development Genesis: Comprehensive ERC-721 NFT implementation with Foundry, including deployment scripts, testing, and Base Sepolia deployment. - script/interactions.s.sol

---
+++ script/interactions.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+
+contract MintBasicNft is Script {
+    string public constant TOKENURI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function run() external {
+        address contractAddress = vm.envAddress("CONTRACT_ADDRESS");
+        mintNftOnContract(contractAddress);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNft(contractAddress).mintNft(TOKENURI);
+        vm.stopBroadcast();
+    }
+}
