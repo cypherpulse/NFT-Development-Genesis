08f8033c761d3fa95d0bcbf614b8cd4a27fd540f
cypherpulse <johnbradill67@gmail.com>
2026-01-03 00:54:15 +0300

    NFT Development Genesis: Comprehensive ERC-721 NFT implementation with Foundry, including deployment scripts, testing, and Base Sepolia deployment. - src/MoodNft.sol

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -26,7 +26,7 @@
 
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 
 contract MoodNft is ERC721{
