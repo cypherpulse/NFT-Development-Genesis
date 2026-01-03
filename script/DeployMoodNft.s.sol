5e3398be75e2583db0d18a49e7ab25ef3360ebcd
cypherpulse <johnbradill67@gmail.com>
2026-01-03 13:11:53 +0300

    Updated Our  Comprehensive ERC-721 NFT implementation with Foundry, including deployment scripts, testing, and Base Sepolia deployment. - script/DeployMoodNft.s.sol

--- script/DeployMoodNft.s.sol
+++ script/DeployMoodNft.s.sol
@@ -7,4 +7,8 @@ import {MoodNft} from "../src/MoodNft.sol";
 
 contract DeployMoodNft is Script {
     function run() external returns (MoodNft){}
+
+    function svgToImageURI(string memory svg) public pure returns (string memory ){
+        string memory baseURL = "data:image/svg+xml;base64,";
+    }
 }
